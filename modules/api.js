9a20e357fb2dc6d947560030dfacfbdff1e3e663
Milana <milana.milana@gmail.com>
2025-04-30 15:07:33 +0300

    api 2 часть

--- modules/api.js
+++ modules/api.js
@@ -16,8 +16,6 @@ export const fetchComments = () => {
             return res.json()
         })
         .then((responseData) => {
-            console.log('Comments from server:', responseData.comments)
-
             const formattedComments = responseData.comments.map((comment) => ({
                 name: comment.author?.name || 'Anonymous',
                 date: comment.date || getCurrentDateTime(),
@@ -28,8 +26,6 @@ export const fetchComments = () => {
 
             comments.length = 0
             comments.push(...formattedComments)
-            console.log('Обновленный массив комментариев:', comments)
-
             return comments
         })
         .catch((error) => {
@@ -39,7 +35,6 @@ export const fetchComments = () => {
 }
 
 export const postComment = (text, name) => {
-    console.log('API sending:', { text, name })
     return fetch(`${host}/comments`, {
         method: 'POST',
         body: JSON.stringify({
@@ -49,17 +44,17 @@ export const postComment = (text, name) => {
     })
         .then((res) => {
             if (!res.ok) {
-                if (res.status === 400) {
-                    return res.json().then((errorData) => {
-                        console.log('Server error response:', errorData)
-                        throw new Error(errorData.error)
-                    })
-                }
-                throw new Error(
-                    `Ошибка сервера при отправке комментария: ${res.status}`,
-                )
+                return res.json().then((errorData) => {
+                    throw new Error(
+                        errorData.error || `Ошибка сервера: ${res.status}`,
+                    )
+                })
             }
             return res.json()
         })
         .then(() => fetchComments())
+        .catch((error) => {
+            console.error('Ошибка при отправке комментария:', error)
+            throw error
+        })
 }
